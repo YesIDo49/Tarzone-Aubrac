28017f06947986e0bae78d827bf7a6dfd4fc6946
lnourry75 <90520293+lnourry75@users.noreply.github.com>
2022-09-22 12:03:27 +0200

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import '../../style.css'
+import FooterImg from "../../assets/images/Group 9.png"
+import Instagram from "../../assets/images/Vector.png"
+import Facebook from "../../assets/images/Vector (1).png"
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+        <div>
+            <img class="footer_img" src={FooterImg} alt=""  />
+        </div>
+        <div class="footer_text">
+            <div class="footer_text_informations">
+                <h3>Nos informations</h3>
+                <p>36 boulevard de Pesaro, 92000</p><br />
+                <p>NANTERRE</p><br />
+                <p>01 40 72 70 00, aubrac@mail.com</p>
+            </div>
+            <div className="footer_text_ouvert">
+                <h3>Ouvert</h3>
+                <p>Lundi au Dimanche: 12h à 14h30 - 19h30 à 23h</p>
+            </div>
+            <div className="footer_text_reseaux">
+                <h3>Suivez nous sur</h3>
+               <a href=""><img src={Instagram} alt=""  /></a>
+               <a href=""><img src={Facebook} alt=""  /></a>
+            </div>
+        </div>
+        </footer>
+
+
     )
 }
 
